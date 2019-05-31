dd7f7d6fda2686c2130bb677a93757a093bb99ea
batona <32574574+batona@users.noreply.github.com>
2019-05-31 12:25:36 +0300

    STM32 compatible
    Connection to STM32F103C board

--- examples/graphicstest/graphicstest.ino
+++ examples/graphicstest/graphicstest.ino
@@ -8,11 +8,19 @@
 #include <Arduino_ST7789.h> // Hardware-specific library for ST7789 (with or without CS pin)
 #include <SPI.h>
 
-#define TFT_DC    8
-#define TFT_RST   9 
-//#define TFT_CS    10 // only for displays with CS pin
-#define TFT_MOSI  11   // for hardware SPI data pin (all of available pins)
-#define TFT_SCLK  13   // for hardware SPI sclk pin (all of available pins)
+//arduino
+//#define TFT_DC    8
+//#define TFT_RST   9
+////#define TFT_CS    10 // only for displays with CS pin
+//#define TFT_MOSI  11   // for hardware SPI data pin (all of available pins)
+//#define TFT_SCLK  13   // for hardware SPI sclk pin (all of available pins)
+
+//stm32duino
+#define TFT_DC    PA2
+#define TFT_RST   PA3
+//#define TFT_CS    PA4 // only for displays with CS pin
+#define TFT_MOSI  PA7   // ==SDA for hardware SPI data pin (all of available pins)
+#define TFT_SCLK  PA5   // ==SCL for hardware SPI sclk pin (all of available pins)
 
 //You can use different type of hardware initialization
 //using hardware SPI (11, 13 on UNO; 51, 52 on MEGA; ICSP-4, ICSP-3 on DUE and etc)
@@ -49,7 +57,7 @@ void setup(void) {
   delay(4000);
 
   // a single pixel
-  tft.drawPixel(tft.width()/2, tft.height()/2, GREEN);
+  tft.drawPixel(tft.width() / 2, tft.height() / 2, GREEN);
   delay(500);
 
   // line draw test
@@ -93,35 +101,35 @@ void loop() {
 
 void testlines(uint16_t color) {
   tft.fillScreen(BLACK);
-  for (int16_t x=0; x < tft.width(); x+=6) {
-    tft.drawLine(0, 0, x, tft.height()-1, color);
+  for (int16_t x = 0; x < tft.width(); x += 6) {
+    tft.drawLine(0, 0, x, tft.height() - 1, color);
   }
-  for (int16_t y=0; y < tft.height(); y+=6) {
-    tft.drawLine(0, 0, tft.width()-1, y, color);
-  }
-
-  tft.fillScreen(BLACK);
-  for (int16_t x=0; x < tft.width(); x+=6) {
-    tft.drawLine(tft.width()-1, 0, x, tft.height()-1, color);
-  }
-  for (int16_t y=0; y < tft.height(); y+=6) {
-    tft.drawLine(tft.width()-1, 0, 0, y, color);
+  for (int16_t y = 0; y < tft.height(); y += 6) {
+    tft.drawLine(0, 0, tft.width() - 1, y, color);
   }
 
   tft.fillScreen(BLACK);
-  for (int16_t x=0; x < tft.width(); x+=6) {
-    tft.drawLine(0, tft.height()-1, x, 0, color);
+  for (int16_t x = 0; x < tft.width(); x += 6) {
+    tft.drawLine(tft.width() - 1, 0, x, tft.height() - 1, color);
   }
-  for (int16_t y=0; y < tft.height(); y+=6) {
-    tft.drawLine(0, tft.height()-1, tft.width()-1, y, color);
+  for (int16_t y = 0; y < tft.height(); y += 6) {
+    tft.drawLine(tft.width() - 1, 0, 0, y, color);
   }
 
   tft.fillScreen(BLACK);
-  for (int16_t x=0; x < tft.width(); x+=6) {
-    tft.drawLine(tft.width()-1, tft.height()-1, x, 0, color);
+  for (int16_t x = 0; x < tft.width(); x += 6) {
+    tft.drawLine(0, tft.height() - 1, x, 0, color);
   }
-  for (int16_t y=0; y < tft.height(); y+=6) {
-    tft.drawLine(tft.width()-1, tft.height()-1, 0, y, color);
+  for (int16_t y = 0; y < tft.height(); y += 6) {
+    tft.drawLine(0, tft.height() - 1, tft.width() - 1, y, color);
+  }
+
+  tft.fillScreen(BLACK);
+  for (int16_t x = 0; x < tft.width(); x += 6) {
+    tft.drawLine(tft.width() - 1, tft.height() - 1, x, 0, color);
+  }
+  for (int16_t y = 0; y < tft.height(); y += 6) {
+    tft.drawLine(tft.width() - 1, tft.height() - 1, 0, y, color);
   }
 }
 
@@ -134,40 +142,40 @@ void testdrawtext(char *text, uint16_t color) {
 
 void testfastlines(uint16_t color1, uint16_t color2) {
   tft.fillScreen(BLACK);
-  for (int16_t y=0; y < tft.height(); y+=5) {
+  for (int16_t y = 0; y < tft.height(); y += 5) {
     tft.drawFastHLine(0, y, tft.width(), color1);
   }
-  for (int16_t x=0; x < tft.width(); x+=5) {
+  for (int16_t x = 0; x < tft.width(); x += 5) {
     tft.drawFastVLine(x, 0, tft.height(), color2);
   }
 }
 
 void testdrawrects(uint16_t color) {
   tft.fillScreen(BLACK);
-  for (int16_t x=0; x < tft.width(); x+=6) {
-    tft.drawRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color);
+  for (int16_t x = 0; x < tft.width(); x += 6) {
+    tft.drawRect(tft.width() / 2 - x / 2, tft.height() / 2 - x / 2 , x, x, color);
   }
 }
 
 void testfillrects(uint16_t color1, uint16_t color2) {
   tft.fillScreen(BLACK);
-  for (int16_t x=tft.width()-1; x > 6; x-=6) {
-    tft.fillRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color1);
-    tft.drawRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color2);
+  for (int16_t x = tft.width() - 1; x > 6; x -= 6) {
+    tft.fillRect(tft.width() / 2 - x / 2, tft.height() / 2 - x / 2 , x, x, color1);
+    tft.drawRect(tft.width() / 2 - x / 2, tft.height() / 2 - x / 2 , x, x, color2);
   }
 }
 
 void testfillcircles(uint8_t radius, uint16_t color) {
-  for (int16_t x=radius; x < tft.width(); x+=radius*2) {
-    for (int16_t y=radius; y < tft.height(); y+=radius*2) {
+  for (int16_t x = radius; x < tft.width(); x += radius * 2) {
+    for (int16_t y = radius; y < tft.height(); y += radius * 2) {
       tft.fillCircle(x, y, radius, color);
     }
   }
 }
 
 void testdrawcircles(uint8_t radius, uint16_t color) {
-  for (int16_t x=0; x < tft.width()+radius; x+=radius*2) {
-    for (int16_t y=0; y < tft.height()+radius; y+=radius*2) {
+  for (int16_t x = 0; x < tft.width() + radius; x += radius * 2) {
+    for (int16_t y = 0; y < tft.height() + radius; y += radius * 2) {
       tft.drawCircle(x, y, radius, color);
     }
   }
@@ -177,16 +185,16 @@ void testtriangles() {
   tft.fillScreen(BLACK);
   int color = 0xF800;
   int t;
-  int w = tft.width()/2;
-  int x = tft.height()-1;
+  int w = tft.width() / 2;
+  int x = tft.height() - 1;
   int y = 0;
   int z = tft.width();
-  for(t = 0 ; t <= 15; t++) {
+  for (t = 0 ; t <= 15; t++) {
     tft.drawTriangle(w, y, y, x, z, x, color);
-    x-=4;
-    y+=4;
-    z-=4;
-    color+=100;
+    x -= 4;
+    y += 4;
+    z -= 4;
+    color += 100;
   }
 }
 
@@ -195,20 +203,20 @@ void testroundrects() {
   int color = 100;
   int i;
   int t;
-  for(t = 0 ; t <= 4; t+=1) {
+  for (t = 0 ; t <= 4; t += 1) {
     int x = 0;
     int y = 0;
-    int w = tft.width()-2;
-    int h = tft.height()-2;
-    for(i = 0 ; i <= 16; i+=1) {
+    int w = tft.width() - 2;
+    int h = tft.height() - 2;
+    for (i = 0 ; i <= 16; i += 1) {
       tft.drawRoundRect(x, y, w, h, 5, color);
-      x+=2;
-      y+=3;
-      w-=4;
-      h-=6;
-      color+=1100;
+      x += 2;
+      y += 3;
+      w -= 4;
+      h -= 6;
+      color += 1100;
     }
-    color+=100;
+    color += 100;
   }
 }
 
